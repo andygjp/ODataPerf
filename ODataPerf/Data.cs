b90d9f93912649745223701a1190e6f292f8afab
andrewpattison <andygjp@hotmail.com>
2023-08-24 11:00:38 +0100

    refactor: reusing data

---
+++ ODataPerf/Data.cs
@@ -0,0 +1,20 @@
+namespace ODataPerf;
+
+public static class Data
+{
+    private static readonly string[] Summaries = 
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static WeatherForecast[] Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
